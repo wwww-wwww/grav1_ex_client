28071885f4a22e222e2aca7ad568a78745bd34b0
w <wvvwvvvvwvvw@gmail.com>
2020-11-08 22:59:39 -0800

    create backwards semaphore for threads instead of built in idle semaphore

--- executor.py
+++ executor.py
@@ -2,12 +2,11 @@
 # https://github.com/python/cpython/blob/master/Lib/concurrent/futures/thread.py
 # with accessible work queue, working list, and weighted jobs
 
-import os, threading, itertools, weakref, types, logging, traceback
+import os, threading, itertools, types, logging, traceback
 from concurrent.futures import _base
 from collections import deque
 from contextlib import contextmanager
 
-_threads_queues = weakref.WeakKeyDictionary()
 _shutdown = False
 # Lock that ensures that new workers are not created while the interpreter is
 # shutting down. Must be held while mutating _threads_queues and _shutdown.
@@ -47,7 +46,7 @@ class _WorkItem(object):
       logging.error(traceback.format_exc())
 
 
-def _worker(executor_reference, tpe):
+def _worker(tpe):
   while True:
     try:
       with tpe.get_job() as work_item:
@@ -55,29 +54,6 @@ def _worker(executor_reference, tpe):
           result = work_item.run()
           work_item.after(result)
 
-          # attempt to increment idle count
-          executor = executor_reference()
-          if executor is not None:
-            executor._idle_semaphore.release()
-
-          del executor
-          continue
-
-        executor = executor_reference()
-        # Exit if:
-        #   - The interpreter is shutting down OR
-        #   - The executor that owns the worker has been collected OR
-        #   - The executor that owns the worker has been shutdown.
-        if _shutdown or executor is None or executor._shutdown:
-          # Flag the executor as shutting down as early as possible if it
-          # is not gc-ed yet.
-          if executor is not None:
-            executor._shutdown = True
-          # Notice other workers
-          with tpe._work_queue_not_empty:
-            tpe._work_queue_not_empty.notify()
-          return
-        del executor
     except:
       logging.error(traceback.format_exc())
 
@@ -89,8 +65,7 @@ class ThreadPoolExecutor(_base.Executor):
     self.queue_lock = threading.Lock()
     self.work_queue = deque()
     self.working = []
-    self._idle_semaphore = threading.Semaphore(0)
-    self._threads = set()
+    self._threads = []
     self._shutdown = False
     self._shutdown_lock = threading.Lock()
     self._thread_name_prefix = ("ThreadPoolExecutor-%d" % self._counter())
@@ -101,7 +76,23 @@ class ThreadPoolExecutor(_base.Executor):
     self._cv = threading.Condition()
     self._work_queue_not_empty = threading.Condition()
 
+    self._workers_value = 0
+    self._workers_lock = threading.Lock()
+    self._workers_available = threading.Condition(self._workers_lock)
+
     self._shutdown_lock = threading.Lock()
+
+  def workers_acquire(self):
+    with self._workers_available:
+      while self._workers_value >= self.max_workers:
+        self._workers_available.wait()
+
+      self._workers_value += 1
+
+  def workers_release(self):
+    with self._workers_available:
+      self._workers_value = max(self._workers_value - 1, 0)
+      self._workers_available.notify()
 
   def submit(self,
              fn,
@@ -126,8 +117,6 @@ class ThreadPoolExecutor(_base.Executor):
           self._work_queue_not_empty.notify()
 
       self._adjust_thread_count()
-
-  submit.__doc__ = _base.Executor.submit.__doc__
 
   def _acquire(self, weight):
     with self._semaphore:
@@ -165,13 +154,16 @@ class ThreadPoolExecutor(_base.Executor):
 
       front = self.work_queue[0]
 
+    self.workers_acquire()
+
     with self.acquire(front.weight) as acquired:
       if not acquired:
         yield None
+        self.workers_release()
         return
 
       with self.queue_lock:
-        if self.work_queue[0] == front:
+        if len(self.work_queue) > 0 and self.work_queue[0] == front:
           self.work_queue.popleft()
           self.working.append(front)
         else:
@@ -179,12 +171,14 @@ class ThreadPoolExecutor(_base.Executor):
           # either from another process or cancel()
           self._release(front.weight)
           yield None
+          self.workers_release()
           return
 
     try:
       yield front
     finally:
       front.after_remove()
+      self.workers_release()
       self.working.remove(front)
       self._release(front.weight)
       del front
@@ -204,15 +198,8 @@ class ThreadPoolExecutor(_base.Executor):
       self._work_queue_not_empty.notify_all()
 
   def _adjust_thread_count(self):
-    # if idle threads are available, don"t spin new threads
-    if self._idle_semaphore.acquire(timeout=0):
-      return
-
-    # When the executor gets lost, the weakref callback will wake up
-    # the worker threads.
-    def weakref_cb(_, tpe=self):
-      with tpe._work_queue_not_empty:
-        tpe._work_queue_not_empty.notify()
+    with self._workers_available:
+      self._workers_available.notify_all()
 
     num_threads = len(self._threads)
     while num_threads < self.max_workers:
@@ -222,12 +209,11 @@ class ThreadPoolExecutor(_base.Executor):
         name=thread_name,
         target=_worker,
         daemon=True,
-        args=(weakref.ref(self, weakref_cb), self),
+        args=[self],
       )
 
       t.start()
-      self._threads.add(t)
-      _threads_queues[t] = self.work_queue
+      self._threads.append(t)
       num_threads = len(self._threads)
 
   def shutdown(self, wait=True):
